0ba9e09ddff1b6a08406e988fc10d294776cfeee
leobot336 <114504872+leobot336@users.noreply.github.com>
2024-11-07 23:28:18 -0300

    Update Calculadora.cs

--- CalculadoraProjeto /Services /Calculadora.cs
+++ CalculadoraProjeto /Services /Calculadora.cs
@@ -1,1 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace CalculadoraProjeto.Services
+{
+    public class Calculadora
+    {
+        private List<string> _historico;
+
+        public Calculadora()
+        {
+            _historico = new List<string>();
+        }
+        public int Somar(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+
+            _historico.Insert(0, $"Resultado: {resultado}");
+            return resultado;
+        }
+
+        public int Subtrair(int num1, int num2)
+        {
+            int resultado = num1 - num2;
+
+            _historico.Insert(0, $"Resultado: {resultado}");
+            return resultado;
+        }
+
+        public int Dividir(int num1, int num2)
+        {
+            int resultado = num1 / num2;
+
+            _historico.Insert(0, $"Resultado: {resultado}");
+            return resultado;
+        }
+
+        public int Multiplicar(int num1, int num2)
+        {
+            int resultado = num1 * num2;
+            
+            _historico.Insert(0, $"Resultado: {resultado}");
+            return resultado;
+        }
+
+        public List<string> Historico()
+        {
+            _historico.RemoveRange(3, _historico.Count - 3);
+
+            return _historico;
+        }
+    }
+}
